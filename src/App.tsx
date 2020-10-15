84c1e969dcf530f8bec1a4c69b381f60ca82339a
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2020-10-14 22:52:56 -0400

    Logos em svg, importacao de fontes e estilizacao global

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import './styles/global.css';
 function App() {
   return (
     <div className="App">
@@ -10,3 +10,4 @@ function App() {
 }
 
 export default App;
+ 
