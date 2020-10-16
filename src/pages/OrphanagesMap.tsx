80dc4f7ae4b19c10bef80ab0280b5c1c43d0131a
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2020-10-15 20:47:01 -0400

    Adicionando mapa e suas extensoes

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import MapMarkerImg from '../images/Logo.svg';
+
+import 'leaflet/dist/leaflet.css';
+
+import '../styles/pages/orphanage-map.css';
 
 function OrphanagesMap(){
   return (
-    <h1>Hello World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={MapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Amazonas</strong>
+          <span>Manaus</span>
+        </footer>
+      </aside>
+
+      <Map center={[-3.0443101,-60.1071927]} zoom={15} style={{width: '100%', height: '100%'}}>
+          <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> 
+      </Map>
+
+      <Link to="" className="create-orphanage">
+          <FiPlus size={32} color="#fff"/>
+        </Link>
+
+    </div>
   );
 }
 
